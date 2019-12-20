f36924ea36b6ae9037db27bd30871bb71a9fda31
Du <Sampan0103314@gmail.com>
2019-12-20 09:35:02 -0500

    updated menu-item component to navigate using withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component';
+
+const Hatpages = () => <h1>Hat Pages</h1>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={Hatpages} />
+      </Switch>
     </div>
   );
 }
