372def00231f45df07323d90bd0b3d6257d98a03
Du <Sampan0103314@gmail.com>
2020-01-23 14:52:52 -0500

    created cart-item component and connected our cart dropdown component

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './cart-item.styles.scss';
+
+const CartItem = ({ imageUrl, name, price, quantity }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt={name} />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span>
+        {quantity} X ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
